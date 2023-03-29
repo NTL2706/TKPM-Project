8c70b89b8cd97d7c13987922ca0f5efcae08267b
MTGH12022021 <loicho0167277@gmail.com>
2023-03-29 10:19:31 +0700

    done api list_pitch and pitch

--- PickPitch/server/routes/api/index.js
+++ PickPitch/server/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const authRouter = require("./authRoute");
 const imgRouter = require("./imgRouter");
+const pitchRouter = require("./pitchRouter");
 
 router.use("/auth", authRouter);
 router.use("/img", imgRouter);
+router.use("/pitchs", pitchRouter);
 
 module.exports = router;
