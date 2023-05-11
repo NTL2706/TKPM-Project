dbbc9293640e241c916e10ac52aa77da5190c6aa
dangminhduc@20127136 <minhducdang020102@gmail.com>
2023-05-11 09:12:09 +0700

    create layout dashboard

--- PickPitch/server/routes/index.js
+++ PickPitch/server/routes/index.js
@@ -2,8 +2,10 @@ const authnticaRoute = require("./authentication/index");
 const apiRoute = require("./api/index");
 const pitchRoute = require("./stadium/index");
 const paymentRoute = require("./payment/index");
+const dashboardRoute = require("./dashboard/index");
 
 function route(app) {
+  app.use("/", dashboardRoute);
   app.use("/auth", authnticaRoute);
   app.use("/api", apiRoute);
   app.use("/payment", paymentRoute);
