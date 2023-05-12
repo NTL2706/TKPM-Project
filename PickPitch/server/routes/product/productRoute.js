2218fc4b2902d73a7304cd4f327b066e30192a3a
MTGH12022021 <loicho0167277@gmail.com>
2023-05-12 19:35:45 +0700

    fix layout and merge

--- PickPitch/server/routes/product/productRoute.js
+++ PickPitch/server/routes/product/productRoute.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const ticketController = require("../../controllers/product/productController");
 
 router.get("/show-ticket", ticketController.showTicket);
-router.get("/delete-ticket/:idTicket");
+router.get("/delete-ticket/:idTicket", ticketController.deleteTicket);
 
 module.exports = router;
